e54e6edc5f9f63370d622285d6c4e8764784b636
fabiobarbazza <fabio.barbazza@dazn.com>
2022-02-03 20:10:47 +0100

    feat: store model

--- src/retraining.py
+++ src/retraining.py
@@ -6,11 +6,14 @@ from pathlib import Path
 
 
 ###################Reading Records#############
-data_filename=str(Path(__file__).parent / 'data/datalocation.txt')
+data_filename=str(Path(__file__).parent.parent / 'data/datalocation.txt')
 with open(data_filename) as f:
-    df_name = f.readlines()
+    df_name = f.read()
+print(df_name)
 
-trainingdata=pd.read_csv(df_name)
+data_filename=str(Path(__file__).parent.parent / 'data' / df_name)
+
+trainingdata=pd.read_csv(data_filename)
 
 
 ##################Re-training a Model#############
@@ -30,11 +33,12 @@ logit.fit(X,y)
 
 
 ############Pushing to Production###################
-model_filename=str(Path(__file__).parent / 'model/deployedmodelname.txt')
+model_filename=str(Path(__file__).parent.parent / 'model/deployedmodelname.txt')
 with open(model_filename) as f:
-    model_name = f.readlines()
+    model_name = f.read()
 
-pickle.dump(logit,model_name)
+model_filename=str(Path(__file__).parent.parent / 'model' / model_name)
+pickle.dump(logit,open(model_filename,'wb'))
 
 
 
